c516e2034ab20c4e7c16a5fee844d340d75c5fc6
Eng-Hussein@linux <hussein.abdallah96@eng-st.cu.edu.eg>
2021-08-12 11:20:46 +0200

    Rendering User list in room

--- src/index.js
+++ src/index.js
@@ -37,6 +37,11 @@ io.on('connection', (socket) => {
       .to(user.room)
       .emit('message', generateMessage('Admin', `${user.username} has joined!`))
 
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    })
+
     callback()
   })
 
@@ -71,6 +76,11 @@ io.on('connection', (socket) => {
         'message',
         generateMessage('Admin', `${user.username} has left!`)
       )
+
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      })
     }
   })
 })
